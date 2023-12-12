b742a16111b039f0f42a16b2decaaac63952b0cf
YaeSakura <kevin1512979396@outlook.com>
2023-12-12 21:30:13 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringSecurity6-Vue3
---
+++ src/test/java/org/eu/yaesakura/springsecurity6vue3/SpringSecurity6Vue3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eu.yaesakura.springsecurity6vue3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6Vue3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
